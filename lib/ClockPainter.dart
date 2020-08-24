83379a21cc7d9c4124cd21124538ce4e7dcc4a52
savagecarol <karthiksharma1411@gmail.com>
2020-08-25 02:50:18 +0530

    claock is ready now

--- lib/ClockPainter.dart
+++ lib/ClockPainter.dart
@@ -5,7 +5,8 @@ import 'package:flutter/rendering.dart';
 
 class ClockPainter extends CustomPainter {
   var dateTime = DateTime.now();
-  
+
+//
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -43,22 +44,45 @@ class ClockPainter extends CustomPainter {
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 16;
 
+
+
+     var dashBrush = Paint()..color = Colors.white
+     ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.butt
+      ..strokeWidth = 2;
+
     canvas.drawCircle(center, radius - 40, fillBrush);
     canvas.drawCircle(center, radius - 40, outlineBrush);
 
-    var hourHandx = centerx + 60 * cos(270 * pi / 180);
-    var hourHandy = centery + 60 * sin(270 * pi / 180);
+    var hourHandx = centerx +
+        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    var hourHandy = centery +
+        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
     canvas.drawLine(center, Offset(hourHandx, hourHandy), hourHandBrush);
 
-    var minHandx = centerx + 80 * cos(90 * pi / 180);
-    var minHandy = centery + 80 * sin(90 * pi / 180);
+    var minHandx = centerx + 80 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandy = centery + 80 * sin(dateTime.minute * 6 * pi / 180);
     canvas.drawLine(center, Offset(minHandx, minHandy), minHandBrush);
 
-    var secHandx = centerx + 80 * cos(180 * pi / 180);
-    var secHandy = centery + 80 * sin(180 * pi / 180);
+    var secHandx = centerx + 80 * cos(dateTime.second * 6 * pi / 180);
+    var secHandy = centery + 80 * sin(dateTime.second * 6 * pi / 180);
     canvas.drawLine(center, Offset(secHandx, secHandy), secondHandBrush);
 
     canvas.drawCircle(center, 16, centerFillBrush);
+
+    var outerCircleRadius = radius;
+    var innerCircleRadius = radius - 14;
+    for (double i = 0; i < 360; i += 12) {
+      var x1 = centerx + outerCircleRadius * cos(i * pi / 180);
+      var y1 = centerx + outerCircleRadius * sin(i * pi / 180);
+
+       var x2 = centerx + innerCircleRadius * cos(i * pi / 180);
+      var y2 = centerx + innerCircleRadius * sin(i * pi / 180);
+
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
+
+
+    }
   }
 
   @override
--- lib/ClockView.dart
+++ lib/ClockView.dart
@@ -1,22 +1,34 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:clock/ClockPainter.dart';
 import 'package:flutter/material.dart';
 
 class ClockView extends StatefulWidget {
-
-
   @override
   _ClockViewState createState() => _ClockViewState();
 }
 
 class _ClockViewState extends State<ClockView> {
   @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-        width: 300 ,
-        height: 300,
+      width: 300,
+      height: 300,
+      child: Transform.rotate(
+        angle: -pi / 2,
         child: CustomPaint(
           painter: ClockPainter(),
         ),
+      ),
     );
   }
 }
